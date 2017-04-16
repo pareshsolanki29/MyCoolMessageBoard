1795afcda5464a43c1228e4a5dad0362deb73700
paresh solanki <bandralites@gmail.com>
2017-04-16 10:01:13 -0400

    finished the functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
 class CommentsController < ApplicationController
+	before_action :find_message
+	before_action :find_comment, only: [:edit, :update, :destroy]
 	
 
 
 	def create
-		@message = Message.find(params[:message_id])
 		@comment = @message.comments.create(comments_params)
 		@comment.user_id = current_user.id
 		if @comment.save
@@ -11,11 +12,37 @@ class CommentsController < ApplicationController
 		else
 			render 'new'
 		end 
+	end
+	
+	def edit
+	end
+	
+	def update
+		if @comment.update(comments_params)
+			redirect_to message_path(@message)
+		else
+			render 'edit'
+		end
+
+	end 
+	
+
+	def destroy
+		@comment.destroy
+		redirect_to message_path(@message)
 	end 
 
 	private
 
-	def comments_params
-		params.require(:comment).permit(:content)
-	end 
+		def comments_params
+			params.require(:comment).permit(:content)
+		end
+
+		def find_message
+			@message = Message.find(params[:message_id])
+		end 
+		
+		def find_comment
+			@comment = @message.comments.find(params[:id])
+		end 
 end

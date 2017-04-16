823c0f65a59a4996c55e7ed456fead8680b81ab0
paresh solanki <bandralites@gmail.com>
2017-04-16 11:48:07 -0400

    finished the structure

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,11 @@
 class MessagesController < ApplicationController
+	
 	before_action :find_message , only: [:show, :edit, :update, :destroy]
-
+    before_action :authenticate_user!,  except: [:index, :show]
 	def index
 		@messages = Message.all.order("created_at DESC")
 	end 
+
 	def show
 
 	end 

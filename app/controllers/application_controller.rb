5c3403044bc86a49f3016e83ee7547ced06d1f2b
paresh solanki <bandralites@gmail.com>
2017-04-18 14:27:51 -0400

    trying testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+   before_filter :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+			def configure_permitted_parameters
+			  devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+			    user_params.permit( :email, :password)
+			  end
+			  devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+                user_params.permit(:first_name,:last_name,:about, :email, :password, :password_confirmation)
+            end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416000413) do
+ActiveRecord::Schema.define(version: 20170416170302) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 20170416000413) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "about"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
